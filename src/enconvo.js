2fbcec50ac5b98a716aeb58eee898ba36ec14dbf
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-06-12 00:11:41 +0800

    feat: 1. wrap node runtime

--- src/enconvo.js
+++ src/enconvo.js
@@ -2,40 +2,41 @@ import jsonrpc from 'jsonrpc-lite';
 import http from 'http';
 
 function result(method, params) {
+    return new Promise((resolve, reject) => {
 
-    const requestData = jsonrpc.request("123", method, params);
+        const requestData = jsonrpc.request("123", method, params);
 
-    const options = {
-        hostname: '127.0.0.1', // 您的服务器地址
-        port: 8080, // 服务器端口
-        path: '', // 请求路径
-        method: 'POST', // 使用 POST 方法
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(JSON.stringify(requestData)),
-        },
-    };
+        const options = {
+            hostname: '127.0.0.1', // 您的服务器地址
+            port: 8080, // 服务器端口
+            path: '', // 请求路径
+            method: 'POST', // 使用 POST 方法
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(JSON.stringify(requestData)),
+            },
+        };
 
-    const req = http.request(options, (res) => {
-        let data = '';
+        const req = http.request(options, (res) => {
+            let data = '';
 
-        res.on('data', (chunk) => {
-            data += chunk;
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                resolve(data)
+            });
         });
 
-        res.on('end', () => {
-            const response = jsonrpc.parse(data);
-            console.log(response);
+        req.on('error', (error) => {
+            reject(error)
         });
-    });
 
-    req.on('error', (error) => {
-        console.error(error);
-    });
+        req.write(JSON.stringify(requestData));
+        req.end();
 
-    req.write(JSON.stringify(requestData));
-    req.end();
-
+    })
 }
 
 export {result};
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {result} from "./enconvo.js";
 
 const args = process.argv.slice(2);
 const {$option} = JSON.parse(args[0]);
-console.log($option.text);
 
 let cookies = `__Secure-1PSID=WwiZu_MTinPnWs8CLduao28QFippa8UrB3R1ZJjLRC2N9zz4Q5f3K2irMn9rohnysFbO1w.`;
 let bot = new Bard(cookies, {
@@ -19,18 +18,38 @@ let bot = new Bard(cookies, {
 
 let conversationId = "some_random_id"; // optional: to make it remember the conversation
 // let response = await bot.ask("how to implement md5 in kotlin?", conversationId); // conversationId is optional
-console.log("begin...");
 
-
+let response = ""
 await bot.askStream(
-    (res) => {
+    async (res) => {
         // invoke "add"
-        result("add", [res])
-        console.log(res);
+        const end = {
+            "type": "text",
+            "value": response,
+        }
+        let resp = await result("stream", [res])
+        console.log(resp)
+        response += res
     }, // returns the response
     $option.text,
     conversationId,
 );
+
+// response = await bot.ask($option.text, conversationId)
+// console.log(response);
+
+const end = {
+    "type": "text",
+    "value": response,
+}
+
+// await result("result", [end])
+
+console.log("begin")
+let resp = await result("result", [response])
+console.log(resp+"kkk")
+
+
 
 process.exit()
 // other code
